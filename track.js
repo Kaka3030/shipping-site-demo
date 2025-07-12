aaae53229abe3b8d7e16665ee3a4aedf91c9970d
Kaka3030 <pettylove03@gmail.com>
2025-07-12 18:36:01 -0500

    Update track.js

--- track.js
+++ track.js
@@ -8,3 +8,11 @@ function track() {
         status.innerHTML = "<p style='color:red;'>Tracking number not found.</p>";
     }
 }
+function generateTracking() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let tracking = "SHIP";
+    for (let i = 0; i < 10; i++) {
+        tracking += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    document.getElementById("trackingDisplay").innerText = tracking;
+}
